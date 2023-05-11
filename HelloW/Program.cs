8185d329573457e5e297e884c0580680f4e5850b
Alin <alin.mich97@gmail.com>
2023-05-11 23:07:19 +0300

    added line

--- HelloW/Program.cs
+++ HelloW/Program.cs
@@ -11,6 +11,7 @@ namespace HelloW
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!2");
         }
     }
 }
